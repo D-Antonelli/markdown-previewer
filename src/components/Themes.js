1192d3c186802da767287c49c23541fc011c1577
Derya Antonelli <derya.antonelli@gmail.com>
2021-03-23 10:25:07 +0000

    Create themes component to set up themes

---
+++ src/components/Themes.js
@@ -0,0 +1,17 @@
+export const lightTheme = {
+  body: "#87b5b5",
+  textHeader: "#4AA3A3",
+  text: "#000000",
+  background: "#F8F6F8",
+  headerBackground: "#F8F6F8",
+  logoColor: "#264653"
+};
+
+export const darkTheme = {
+  body: "#000000",
+  textHeader: "#264653",
+  text: "#F8F6F8",
+  background: "#000000",
+  headerBackground: "#264653",
+  logoColor: "#F8F6F8"
+};
