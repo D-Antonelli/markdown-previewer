1a281ba5ff0a20817029e13780713fdd304e2e95
Derya Antonelli <derya.antonelli@gmail.com>
2021-03-29 15:30:32 +0100

    Move theme state to the parent component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import './App.css';
-//import React, { useState, useEffect } from 'react';
-import Header from './components/header';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./components/globalStyles";
+import { lightTheme, darkTheme } from "./components/Themes";
+
+import Header from "./components/header";
 
 function App() {
+  const [theme, setTheme] = useState("light");
+
+  const setMode = (mode) => {
+    window.localStorage.setItem("theme", mode);
+    setTheme(mode);
+  };
+
+  const themeToggler = () => {
+    theme === "light" ? setMode("dark") : setMode("light");
+  };
+
+  useEffect(() => {
+    const localTheme = window.localStorage.getItem("theme");
+    localTheme && setTheme(localTheme);
+  }, []);
+
   return (
-    <Header></Header>
-  )
+    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+      <GlobalStyles />
+      <Header onClick={themeToggler} toggleTheme={theme} />
+    </ThemeProvider>
+  );
 }
 
 export default App;
