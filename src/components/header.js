536c961ee1937e071a2fe11ef0cee4d5cea2be18
Derya Antonelli <derya.antonelli@gmail.com>
2021-03-29 15:31:29 +0100

    Move theme switch functionality from header to parent

--- src/components/header.js
+++ src/components/header.js
@@ -1,39 +1,32 @@
-//import React, { useState, useEffect } from "react";
-import { ThemeProvider } from "styled-components";
-import { GlobalStyles } from "./globalStyles";
-import { lightTheme, darkTheme } from "./Themes";
-
 //icons
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMarkdown } from "@fortawesome/free-brands-svg-icons";
-import { faMoon } from "@fortawesome/free-solid-svg-icons";
+import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
 
-function Header() {
+const Header = ({ onClick, toggleTheme }) => {
   return (
-    <ThemeProvider theme={lightTheme}>
-      <>
-      <GlobalStyles/>
-      <header>
-        <div id="header">
-          <div id="header-logo">
-            <div id="logo-icon">
-              <FontAwesomeIcon icon={faMarkdown}></FontAwesomeIcon>
-            </div>
-            <h1 id="logo-text">
-              <span id="logo-text--1">markdown</span>
-              <span id="logo-text--2" class="text">
-                previewer
-              </span>
-            </h1>
+    <header>
+      <div id="header">
+        <div id="header-logo">
+          <div id="logo-icon">
+            <FontAwesomeIcon icon={faMarkdown}></FontAwesomeIcon>
           </div>
-          <button id="theme-toggle">
-              <FontAwesomeIcon icon={faMoon} id="toggle-icon"></FontAwesomeIcon>
-          </button>
+          <h1 id="logo-text">
+            <span id="logo-text--1">markdown</span>
+            <span id="logo-text--2" className="text">
+              previewer
+            </span>
+          </h1>
         </div>
-      </header>
-      </>
-    </ThemeProvider>
+        <button id="theme-toggle" onClick={onClick}>
+          <FontAwesomeIcon
+            icon={toggleTheme === "light" ? faMoon : faSun}
+            id="toggle-icon"
+          ></FontAwesomeIcon>
+        </button>
+      </div>
+    </header>
   );
-}
+};
 
 export default Header;
