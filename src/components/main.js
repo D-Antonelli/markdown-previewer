43644658061f4e99eeff623a903953f4e42aef3d
Derya Antonelli <derya.antonelli@gmail.com>
2021-03-29 17:09:00 +0100

    Initial commit

---
+++ src/components/main.js
@@ -0,0 +1,11 @@
+const Main = () => {
+    return (
+        <div id="main-container">
+            <div className="edit collapseEditor collapse show"></div>
+            <div className="column collapseEditor collapsePreview collapse show"></div>
+            <div className="preview collapse show collapsePreview"></div>
+        </div>
+    );
+}
+
+export default Main;
