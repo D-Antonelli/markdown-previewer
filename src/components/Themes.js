fd22cfa32d73258771e896aac50ac3c41cdaac17
Derya Antonelli <derya.antonelli@gmail.com>
2021-04-14 10:23:21 +0100

    Add two theme components

--- src/components/Themes.js
+++ src/components/Themes.js
@@ -4,7 +4,8 @@ export const lightTheme = {
   text: "#000000",
   background: "#F8F6F8",
   headerBackground: "#F8F6F8",
-  logoColor: "#264653"
+  logoColor: "#264653",
+  tableColor: "#f6f8fa",
 };
 
 export const darkTheme = {
@@ -13,5 +14,6 @@ export const darkTheme = {
   text: "#F8F6F8",
   background: "#000000",
   headerBackground: "#264653",
-  logoColor: "#F8F6F8"
+  logoColor: "#F8F6F8",
+  tableColor: "#000000"
 };
